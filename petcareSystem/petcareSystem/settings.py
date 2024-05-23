ac0f552f09f0a3fe8340862802e893d2e9164d1e
Melika Sadegh <meli.sadegh@gmail.com>
2024-05-23 09:18:04 +0330

    section 2 completed

--- petcareSystem/petcareSystem/settings.py
+++ petcareSystem/petcareSystem/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'petcareSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'petcaresystem_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'Host': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
